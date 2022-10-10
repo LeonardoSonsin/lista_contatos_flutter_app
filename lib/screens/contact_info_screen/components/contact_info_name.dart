0a1b566ae7f35c8b75bb5561739c2a553df2dcaa
Leonardo Sonsin <leonardo.sonsin@blink.com.br>
2022-10-10 11:39:07 -0300

    Novas implementações e correções

--- lib/screens/contact_info_screen/components/contact_info_name.dart
+++ lib/screens/contact_info_screen/components/contact_info_name.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:lista_contatos_flutter_app/themes/my_colors.dart';
 
-class ContactInfoName extends StatelessWidget {
-  const ContactInfoName({Key? key, required this.name}) : super(key: key);
+class ContactInfoNamePhone extends StatelessWidget {
+  const ContactInfoNamePhone({Key? key, required this.name, required this.phone}) : super(key: key);
 
   final String name;
+  final String phone;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8.0, bottom: 18.0),
-      child: Text(
-        name,
-        style: const TextStyle(fontSize: 40),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 5),
+          child: Text(
+            name,
+            style: TextStyle(fontSize: 40, color: MyColors().white),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 5, bottom: 20),
+          child: Text(
+            phone,
+            style: TextStyle(fontSize: 18, color: MyColors().greyText),
+          ),
+        ),
+      ],
     );
   }
 }
